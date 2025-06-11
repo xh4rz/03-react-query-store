293056239cc9d21d1967958f675f75eb2f9ec5d2
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-06-11 04:09:12 -0500

    Mostrar los productos

--- src/products/components/ProductList.tsx
+++ src/products/components/ProductList.tsx
@@ -1,25 +1,15 @@
-import { ProductCard } from ".."
+import { type Product, ProductCard } from '..';
 
-export const ProductList = () => {
-  return (
-    <div className="mt-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-2 justify-center max-w-max">
-
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-
-    </div>
-  )
+interface Props {
+	products: Product[];
 }
+
+export const ProductList = ({ products }: Props) => {
+	return (
+		<div className="mt-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-2 justify-center max-w-max">
+			{products.map((product) => (
+				<ProductCard key={product.id} product={product} />
+			))}
+		</div>
+	);
+};
--- src/products/pages/CompleteListPage.tsx
+++ src/products/pages/CompleteListPage.tsx
@@ -3,6 +3,8 @@ import { ProductList, useProducts } from '..';
 export const CompleteListPage = () => {
 	const { isLoading, products } = useProducts({});
 
+	if (!products) return;
+
 	return (
 		<div className="flex-col">
 			<h1 className="text-2xl font-bold">Todos los productos</h1>
