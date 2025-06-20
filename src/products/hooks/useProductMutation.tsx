c8b198441aeac4ac5fac0b7f81634ac6fbe5f478
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-06-20 02:48:39 -0500

    Evitar invalidar el query

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { productActions } from '..';
+import { Product, productActions } from '..';
 
 export const useProductMutation = () => {
 	const queryClient = useQueryClient();
@@ -7,9 +7,18 @@ export const useProductMutation = () => {
 	const mutation = useMutation({
 		mutationFn: productActions.createProduct,
 		onSuccess: (data) => {
-			queryClient.invalidateQueries({
-				queryKey: ['products', { filterkey: data.category }]
-			});
+			// queryClient.invalidateQueries({
+			// 	queryKey: ['products', { filterkey: data.category }]
+			// });
+
+			queryClient.setQueryData<Product[]>(
+				['products', { filterkey: data.category }],
+				(oldData: Product[] | undefined) => {
+					if (!oldData) return [data];
+
+					return [...oldData, data];
+				}
+			);
 		}
 	});
 
