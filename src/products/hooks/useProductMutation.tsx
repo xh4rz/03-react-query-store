c148763b16ceeb252a73acaf9a489457bdce0828
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-06-20 02:58:30 -0500

    Optimistic Updates

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -6,6 +6,24 @@ export const useProductMutation = () => {
 
 	const mutation = useMutation({
 		mutationFn: productActions.createProduct,
+		onMutate: (data) => {
+			console.log('Mutando - Optimistic update');
+
+			// Optimistic Product
+			const optimisticProduct = { id: Math.random(), ...data };
+
+			console.log({ optimisticProduct });
+
+			// Almacenar el producto en el cache del query client
+			queryClient.setQueryData<Product[]>(
+				['products', { filterkey: data.category }],
+				(oldData) => {
+					if (!oldData) return [optimisticProduct];
+
+					return [...oldData, optimisticProduct];
+				}
+			);
+		},
 		onSuccess: (data) => {
 			// queryClient.invalidateQueries({
 			// 	queryKey: ['products', { filterkey: data.category }]
